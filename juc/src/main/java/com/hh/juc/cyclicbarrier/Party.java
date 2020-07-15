822e7c192a0edf26505b6d703362fde506060fba
LeslieHao <imHaoShuai@163.com>
2020-07-15 15:31:09 +0800

    BarrierAction

--- juc/src/main/java/com/hh/juc/cyclicbarrier/Party.java
+++ juc/src/main/java/com/hh/juc/cyclicbarrier/Party.java
@@ -1,6 +1,7 @@
 package com.hh.juc.cyclicbarrier;
 
 import com.hh.juc.ThreadPool;
+import lombok.SneakyThrows;
 
 import java.util.concurrent.CyclicBarrier;
 
@@ -11,7 +12,7 @@ import java.util.concurrent.CyclicBarrier;
 public class Party {
 
     // party 一共邀请了三个嘉宾
-    static final CyclicBarrier HOST = new CyclicBarrier(4);
+    static final CyclicBarrier HOST = new CyclicBarrier(4, new BarrierAction());
 
     public static void main(String[] args) {
         ThreadPool.get().submit(new PopStar(HOST, "张国荣", 1000L));
@@ -19,4 +20,17 @@ public class Party {
         ThreadPool.get().submit(new PopStar(HOST, "梁朝伟", 3000L));
         ThreadPool.get().submit(new PopStar(HOST, "王家卫", 4000L));
     }
+
+    // BarrierAction 执行完成之后屏障后的操作才会执行
+    static class BarrierAction implements Runnable {
+
+        @SneakyThrows
+        @Override
+        public void run() {
+            System.out.println("所有明星已经到场~");
+            System.out.println("party 准备开始~");
+            Thread.sleep(1000);
+        }
+    }
+
 }
